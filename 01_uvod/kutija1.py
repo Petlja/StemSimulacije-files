4000c90529e26706967cff4c58340b86089231ff
vasiljevic <nebojsa.vasiljevic@gmail.com>
2021-03-14 22:18:56 +0100

    preimenovan folder 01_uvod

--- 01_uvod/kutija1.py
+++ 01_uvod/kutija1.py
@@ -2,16 +2,16 @@ from simanim import *
 
 def setup(m):
     PixelsPerUnit(10)
-    ViewBox((0, 0),50, 20)
+    ViewBox((0, 0),80, 30)
     m.x = 0
     m.y = 0
     m.v = 10
 
 def update(m):
-    m.x = m.x + m.v * m.dt
+    m.x += m.v * m.dt
 
 def draw(m):
-    kutija = Box((m.x,m.y), 10,5)
+    kutija = Box((m.x,m.y), 10, 5)
     kutija.fill_color = '#000000'
     Draw(kutija)
 
