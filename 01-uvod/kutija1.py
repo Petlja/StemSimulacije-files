b7ec2846baff1e78dfea2fc6a283157c2b68d3c0
vasiljevic <nebojsa.vasiljevic@gmail.com>
2021-03-14 15:38:55 +0100

    inicijalni commit

---
+++ 01-uvod/kutija1.py
@@ -0,0 +1,18 @@
+from simanim import *
+
+def setup(m):
+    PixelsPerUnit(10)
+    ViewBox((0, 0),50, 20)
+    m.x = 0
+    m.y = 0
+    m.v = 10
+
+def update(m):
+    m.x = m.x + m.v * m.dt
+
+def draw(m):
+    kutija = Box((m.x,m.y), 10,5)
+    kutija.fill_color = '#000000'
+    Draw(kutija)
+
+Run(setup, update, draw)
