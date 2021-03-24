98c8747cee98c63890371b443c0a77f1651db6be
vugdelija <milanvugdelija@yahoo.com>
2021-03-24 14:27:57 +0100

    Update kutija.py

--- 01_uvod/kutija.py
+++ 01_uvod/kutija.py
@@ -2,7 +2,7 @@ from simanim import *
 
 def setup(m):
     PixelsPerUnit(10)
-    ViewBox((0, 0),60, 20)
+    ViewBox((0, 0), 60, 20)
     m.x = 0
     m.y = 0
     m.v = 10
@@ -13,7 +13,7 @@ def update(m):
 
 def draw(m):
     kutija = Box((m.x,m.y), 10, 5)
-    kutija.fill_color = '#000000'
+    kutija.fill_color = '##ff8800'
     Draw(kutija)
 
 Run(setup, update, draw)
