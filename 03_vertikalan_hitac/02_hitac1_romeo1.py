0232edef494f572fc92baa757a51fe72a82d8e6b
vugdelija <milanvugdelija@yahoo.com>
2021-03-30 17:06:32 +0200

    Update 02_hitac1_romeo1.py

--- 03_vertikalan_hitac/02_hitac1_romeo1.py
+++ 03_vertikalan_hitac/02_hitac1_romeo1.py
@@ -2,37 +2,39 @@ import math
 from simanim import *
 
 # konstante:
-h0 = 1.2 # Visina Romeove ruke
-h1 = 2.8 # Visina Julijine ruke
+scena_w, scena_h = 2.56, 4
+ruza_w, ruza_h = 0.25, 0.4
+
+h0 = 1.2 # Visina Romeove sake
+h1 = 2.8 # Visina Julijine sake
 
 def setup(m):
     PixelsPerUnit(100)
-    ViewBox((0, 0), 2.56, 4)
+    ViewBox((0, 0), 2.6, 4)
     FramesPerSecond(60)
     UpdatesPerFrame(10)
 
     m.v0 = InputFloat(7.1, (1, 9))
 
-    m.ruza_img, m.m, (m.ruza_w, m.ruza_h) = "rose.png", 0.5, (0.25, 0.4)
-    m.scena_img, m.scena_w, m.scena_h = "romeo1.png", 2.56, 4
+    m.h = h0    # m.h ce biti visina sredista ruze
+    m.v = m.v0  # m.v ce biti brzina ruze
+    m.g = 10    # koristimo g = 10 zbog jednostavnijeg racunanja
 
-    m.h = h0
-    m.v = m.v0
-    m.g = 10 # koristimo g = 10 zbog jednostavnijeg racunanja
 
 def update(m):
-    m.h = max(h0, m.h + m.v * m.dt)
+    m.h = max(h0, m.h + m.v * m.dt) # ne nize od Romeove ruke
     m.v -= m.g * m.dt 
 
-    # drugi deo uslova nema veze sa teorijom i formulama iz fizike
-    # radi se o proceni: koliko daleko ruza sme da bude i koliko brzo sme 
+    # brojevi u uslovu su odabrani procenom
+    # koliko daleko ruza sme da bude i koliko brzo sme 
     # da se krece da bi Julija mogla da je uhvati
-    if m.h == h0 or (abs(m.h - h1) < m.ruza_h/5 and abs(m.v - -0.8) < 0.3):
+    if m.h == h0 or (abs(m.h - h1) < ruza_h/5 and abs(m.v - -0.8) < 0.3):
         Finish()
 
+
 def draw(m):
-    scena = Image(m.scena_img, (0, 0), m.scena_w, m.scena_h)
-    ruza = Image(m.ruza_img, (1.45, m.h - m.ruza_h/2), m.ruza_w, m.ruza_h)
+    scena = Image("romeo1.png", (0, 0), scena_w, scena_h)
+    ruza = Image("rose.png", (1.6 - ruza_w/2, m.h - ruza_h/2), ruza_w, ruza_h)
 
     Draw(scena, ruza)
 
